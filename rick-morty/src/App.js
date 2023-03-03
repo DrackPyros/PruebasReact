30624df797e937fb0c25cdd9b8d65b437cc4b366
DrackPyros <santiagojovaniss@gmail.com>
2023-03-03 19:30:18 +0100

    Rick y morty

---
+++ rick-morty/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import imgRickMorty from './img/rick-morty.png'
+import './App.css';
+import { useState } from 'react';
+import Characters from './components/Characters';
+
+function App() {
+  const [ characters, setCharacters ] = useState(null);
+  const regApi = async () => {
+    const api = await fetch('https://rickandmortyapi.com/api/character');
+    // console.log(api);
+    const characterApi = await api.json();
+
+    setCharacters(characterApi.results);
+  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className='title'>Rick & Morty</h1>
+        { characters ? (
+          <Characters characters = {characters} setCharacters={setCharacters}/>
+        ): (
+          <>
+            <img src={imgRickMorty} alt="Rick & Morty" className='img-home' />
+            <button onClick={regApi} className="btn-search">Buscar Personajes</button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
